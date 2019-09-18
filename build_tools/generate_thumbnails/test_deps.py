a969a5be7c7ee0794651ddc224c4046266d7577c
Patrik Turi <patrik.turi.0xff@gmail.com>
2019-09-18 21:50:36 +0200

    Refactor build tool tests to make them more readable

--- build_tools/generate_thumbnails/test_deps.py
+++ build_tools/generate_thumbnails/test_deps.py
@@ -11,7 +11,7 @@ INPUT_DIR_PATH = '../images'
 
 TEST_CONFIGS = {
 	'make': ('make', 'out', ['make'], False),
-	'gradle': ('gradle', 'build', ['gradlew.bat', 'thumb'], True),
+	'gradle': ('gradle', 'build', ['gradle.bat', 'thumb'], True),
 	'doit': ('doit', 'out', ['.venv/Scripts/doit.exe'], True),
 }
 
@@ -24,7 +24,7 @@ CONTENT_BASED = False
 class TestDeps(unittest.TestCase):
 
 	def test_build_deps(self):
-		# ---- Build from scratch, check files exist (scratch)
+		# ---- Build from scratch, check files exist
 		out_dir_path = OUT_DIR_PATH
 		command = BUILD_COMMAND
 		# Prepare
@@ -32,90 +32,91 @@ class TestDeps(unittest.TestCase):
 		if os.path.exists(out_dir_path):
 			shutil.rmtree(out_dir_path)
 		
+		out_png_dir_path = os.path.join(out_dir_path, 'thumb')
+		self.png1_path = png1_path = os.path.join(out_png_dir_path, '1.png')
+		self.png2_path = png2_path = os.path.join(out_png_dir_path, '2.png')
+		self.files_txt_path = files_txt_path = os.path.join(out_dir_path, 'files.txt')
+		
+		self.update_timestamps(init=True)
+
 		# Run
 		subprocess.run(command, check=True)
 		
 		# Assert
-		out_png_dir_path = os.path.join(out_dir_path, 'thumb')
-		png1_path = os.path.join(out_png_dir_path, '1.png')
-		png2_path = os.path.join(out_png_dir_path, '2.png')
+		self.update_timestamps()
 		self.assert_files_exist(out_png_dir_path, ['1.png', '2.png', '3.png'])
-
-		files_txt_path = os.path.join(out_dir_path, 'files.txt')
 		self.assertTrue(os.path.isfile(files_txt_path))
 
-		png2_prev_mtime = os.path.getmtime(png2_path)
-
 		print('== Build from scratch OK')
-
-		# ---- Remove files.txt - only files.txt shall be regenerated (down)
+		# ---- Remove files.txt - files.txt shall be regenerated
 		# Sleep needed between make commands to make sure outdated timestamps can be detected
-		time.sleep(1)
+		if not CONTENT_BASED:
+			time.sleep(1)
+		
 		os.remove(files_txt_path)
 		
 		subprocess.run(command, check=True)
 		
-		png2_mtime = os.path.getmtime(png2_path)
-		self.assertEqual(png2_mtime, png2_prev_mtime)
+		self.update_timestamps()
+		self.assertEqual(self.png2_mtime, self.png2_prev_mtime)
 		self.assertTrue(os.path.isfile(files_txt_path))
 
-		png2_prev_mtime = png2_mtime
-
 		print('== files.txt regen OK')
-		
-		time.sleep(1)
+		# ---- Touch images/2.png -- only 2.png and files.txt shall be regenerated
+		# TODO: test - touch out/2.png - only 2.png and files.txt are regenerated!
+		if not CONTENT_BASED:
+			time.sleep(1)
 
 		png2_input_path = os.path.join(INPUT_DIR_PATH, '2.png')
-		os.utime(png2_input_path, None)
+		self.mod_file(png2_input_path)
 
 		subprocess.run(command, check=True)
 
-		png2_mtime = os.path.getmtime(png2_path)
-		self.assertGreater(png2_mtime, png2_prev_mtime)
-
-		filestxt_prev_mtime = os.path.getmtime(files_txt_path)
+		self.update_timestamps()
+		# png 1 shall not be regenerated
+		self.assertEqual(self.png1_mtime, self.png1_prev_mtime, '1.png shall not be regenerated because only 2.png was changed')
+		self.assertGreater(self.png2_mtime, self.png2_prev_mtime)
+		self.assertGreater(self.filestxt_mtime, self.filestxt_prev_mtime)
 
 		print('== touch top level .png regen OK')
+		# ---- Touch files.txt - only files.txt shall be regenerated
+		if not CONTENT_BASED:
+			time.sleep(1)
 
-		time.sleep(1)
-
-		os.utime(png2_path, None)
+		self.mod_file(files_txt_path)
 		
 		subprocess.run(command, check=True)
 
-		filestxt_mtime = os.path.getmtime(files_txt_path)
-		if not CONTENT_BASED:
-			self.assertGreater(filestxt_mtime, filestxt_prev_mtime)
-		# else: TODO: alter file for content based engines
-		
-		png1_prev_mtime = os.path.getmtime(png1_path)
-		filestxt_prev_mtime = filestxt_mtime
+		self.update_timestamps()
+		self.assertEqual(self.png1_mtime, self.png1_prev_mtime, '1.png shall not be regenerated because only files.txt was changed')
+		self.assertGreater(self.filestxt_mtime, self.filestxt_prev_mtime)
 
 		print('== touch files.txt regen OK')
 
-		time.sleep(1)
-		# ---- Remove input file 2.png, files.txt shall be regenerated (up)
-		os.remove(png2_path)
-		subprocess.run(command, check=True)
+	def update_timestamps(self, init=False):
+		if not init:
+			self.png1_prev_mtime = self.png1_mtime
+			self.png2_prev_mtime = self.png2_mtime
+			self.filestxt_prev_mtime = self.filestxt_mtime
 		
-		png1_mtime = os.path.getmtime(png1_path)
-		# png 1 shall not be regenerated
-		self.assertEqual(png1_mtime, png1_prev_mtime)
-
-		# png 2 shall be generated
-		self.assertTrue(os.path.isfile(png2_path))
-		
-		# files.txt shall be regenerated
-		filestxt_mtime = os.path.getmtime(files_txt_path)
-		self.assertGreater(filestxt_mtime, filestxt_prev_mtime)
-		
-		print('== only png 2 regen OK')
+		self.png1_mtime = None if init else os.path.getmtime(self.png1_path)
+		self.png2_mtime = None if init else os.path.getmtime(self.png2_path)
+		self.filestxt_mtime = None if init else os.path.getmtime(self.files_txt_path)
 
 	def assert_files_exist(self, dir_path, file_names):
 		for f in file_names:
 			file_path = os.path.join(dir_path, f)
 			self.assertTrue(os.path.isfile(file_path))
 
+	def mod_file(self, file_path):
+		if CONTENT_BASED:
+			self.mod_bin(file_path)
+		else:
+			os.utime(file_path, None)
+
+	def mod_bin(self, file_path):
+		with open(file_path, 'ab') as file:
+			file.write(bytearray([0x11, 0x11, 0x11, 0x11]))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
