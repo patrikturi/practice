a6412993caa3cb32bb8a81602c14f3b62fcfb439
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-02-08 17:57:30 +0100

    Refactor logic

--- unit_testing/trivia/python3/trivia.py
+++ unit_testing/trivia/python3/trivia.py
@@ -37,10 +37,7 @@ class Game:
             self.pop_questions.append("Pop Question %s" % i)
             self.science_questions.append("Science Question %s" % i)
             self.sports_questions.append("Sports Question %s" % i)
-            self.rock_questions.append(self.create_rock_question(i))
-
-    def create_rock_question(self, index):
-        return "Rock Question %s" % index
+            self.rock_questions.append("Rock Question %s" % i)
 
     def is_playable(self):
         return self.how_many_players >= 2
@@ -69,47 +66,47 @@ class Game:
                     '\'s new location is ' + \
                     str(self.places[self.current_player]))
 
-    def set_getting_out(self, is_true):
-        self.is_getting_out_of_penalty_box = is_true
-        is_true_str = '' if is_true else 'not '
-        self.logger.print("%s is %sgetting out of the penalty box" % (self.players[self.current_player], is_true_str))
-
     def roll(self, roll):
         self.logger.print("%s is the current player" % self.players[self.current_player])
         self.logger.print("They have rolled a %s" % roll)
 
         if self.in_penalty_box[self.current_player]:
-            if roll % 2 != 0:
-                self.set_getting_out(True)
+            self.is_getting_out_of_penalty_box = roll % 2 != 0
+            negate = '' if self.is_getting_out_of_penalty_box else 'not '
+            self.logger.print("%s is %sgetting out of the penalty box" % (self.players[self.current_player], negate))
 
-                self.step_player(roll)
+            if not self.is_getting_out_of_penalty_box:
+                return
 
-                self._ask_question()
-            else:
-                self.set_getting_out(False)
-        else:
-            self.step_player(roll)
+        self.step_player(roll)
 
-            self._ask_question()
+        self._ask_question()
 
     def _ask_question(self):
         self.logger.print("The category is %s" % self._current_category)
-        if self._current_category == 'Pop': self.logger.print(self.pop_questions.pop(0))
-        if self._current_category == 'Science': self.logger.print(self.science_questions.pop(0))
-        if self._current_category == 'Sports': self.logger.print(self.sports_questions.pop(0))
-        if self._current_category == 'Rock': self.logger.print(self.rock_questions.pop(0))
+        if self._current_category == 'Pop': 
+            questions = self.pop_questions
+        if self._current_category == 'Science':
+            questions = self.science_questions
+        if self._current_category == 'Sports':
+            questions = self.sports_questions
+        if self._current_category == 'Rock':
+            questions = self.rock_questions
+
+        self.logger.print(questions.pop(0))
 
     @property
     def _current_category(self):
-        if self.places[self.current_player] == 0: return 'Pop'
-        if self.places[self.current_player] == 4: return 'Pop'
-        if self.places[self.current_player] == 8: return 'Pop'
-        if self.places[self.current_player] == 1: return 'Science'
-        if self.places[self.current_player] == 5: return 'Science'
-        if self.places[self.current_player] == 9: return 'Science'
-        if self.places[self.current_player] == 2: return 'Sports'
-        if self.places[self.current_player] == 6: return 'Sports'
-        if self.places[self.current_player] == 10: return 'Sports'
+        position = self.places[self.current_player]
+        if position == 0: return 'Pop'
+        if position == 4: return 'Pop'
+        if position == 8: return 'Pop'
+        if position == 1: return 'Science'
+        if position == 5: return 'Science'
+        if position == 9: return 'Science'
+        if position == 2: return 'Sports'
+        if position == 6: return 'Sports'
+        if position == 10: return 'Sports'
         return 'Rock'
 
     def next_player(self):
@@ -126,25 +123,18 @@ class Game:
             ' Gold Coins.')
 
     def was_correctly_answered(self):
-        if self.in_penalty_box[self.current_player]:
-            if self.is_getting_out_of_penalty_box:
-                self.correct_answer()
+        if self.in_penalty_box[self.current_player] \
+            and not self.is_getting_out_of_penalty_box:
 
-                winner = self._did_player_win()
-                self.next_player()
-
-                return winner
-            else:
-                self.next_player()
-                return True
-
-        else:
-            self.correct_answer()
-
-            winner = self._did_player_win()
             self.next_player()
+            return False
 
-            return winner
+        self.correct_answer()
+
+        winner = self._did_player_win()
+        self.next_player()
+
+        return winner
 
     def wrong_answer(self):
         self.logger.print('Question was incorrectly answered')
@@ -152,24 +142,22 @@ class Game:
         self.in_penalty_box[self.current_player] = True
 
         self.next_player()
-        return True
 
     def _did_player_win(self):
-        return not (self.purses[self.current_player] == 6)
+        return self.purses[self.current_player] == 6
 
     def play(self, seed_value):
 
-        no_winner = True
+        winner_found = False
         seed(seed_value)
 
-        while no_winner:
+        while not winner_found:
             self.roll(randrange(5) + 1)
 
             if randrange(9) == 7:
-                no_winner = self.wrong_answer()
+                self.wrong_answer()
             else:
-                no_winner = self.was_correctly_answered()
-
+                winner_found = self.was_correctly_answered()
 
 
 if __name__ == '__main__':
